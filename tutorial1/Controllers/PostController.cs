4d695723ef873adc314a18ba093ca1a6dff256d0
nminhduc <nguyen.minh.duc@framgia.com>
2016-06-18 06:51:32 +0700

    Add project files.

---
+++ tutorial1/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tutorial1.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Hello world
+
+        public string Index()
+        {
+            return "this is my <b> Default - InDex <b> action ...";
+        }
+        // GET: Hello world/welcome
+        public string Welcome()
+        {
+            return "this is welcome action method";
+        }
+    }
+}
